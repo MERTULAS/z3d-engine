ee541ae84f016d260f65b42af8bc96aa1d74d56b
Mert ULAS <h.mert.ulas@gmail.com>
2022-02-01 01:25:59 +0300

    Update main.cpp

--- Zed Engine/src/main.cpp
+++ Zed Engine/src/main.cpp
@@ -5,7 +5,7 @@ Zed_Engine* game = nullptr;
 int main(int argc, char* argv[])
 {
 	game = new Zed_Engine();
-	game->Init("SELAMLAR", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, false);
+	game->Init("Z3D Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, false);
 
 	while (game->Running()) {
 		game->HandleEvents();
